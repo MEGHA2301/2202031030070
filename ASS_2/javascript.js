3d01765222e43a59806ccbae503352cdbdf3ab82
MEGHA2301 <137033619+MEGHA2301@users.noreply.github.com>
2023-07-15 11:05:59 +0530

    Add files via upload

---
+++ ASS_2/javascript.js
@@ -0,0 +1,42 @@
+function validationEvent(){
+    var Fname = document.getElementById("name").value;
+    var Em_1 = document.getElementById("email").value;
+    var Ph = document.getElementById("phone").value;
+    var Msg = document.getElementById("message").value;
+
+
+    if(Fname == ""){
+        alert("please Enter FullName...")
+        return false;
+    }
+    else if (Fname.search(/^([a-zA-Z ]{2,40})$/) == -1) {
+        alert("Please Enter valid FullName..")
+        return false;
+    }
+    else if (Em_1 == "") {
+        alert("Please Enter Email..")
+        return false;
+    }
+    else if (Em_1.search(/^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/) == -1) {
+        alert("Please Enter valid Email..")
+        return false;
+    }
+    else if (Ph == "") {
+        alert("Please Enter Phonenumber..")
+        return false;
+    }
+    else if (Ph.search(/^[6-9][0-9]{9}$/) == -1) {
+        alert("Please Enter valid PhoneNumber..")
+        return false;
+    }
+    else if (Msg == "") {
+        alert("Please Enter Your Message..")
+        return false;
+    
+    }
+    else{
+        alert("submitted..")
+        return (true)
+    }
+}
+
